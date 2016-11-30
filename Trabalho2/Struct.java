b7f172710f4cf09c42f732c03baf79b8fcdd00b9
wagnervieirafilho <waguinhosanjos@gmail.com>
2016-11-30 17:28:32 -0200

    Implementação dos métodos

--- Trabalho2/Struct.java
+++ Trabalho2/Struct.java
@@ -1,13 +1,32 @@
 class Struct{
-	int id;
-	int task;
-	Assentos[] assentos;
+	private int id;
+	private int task;
+	private int[] assentos;
 
 	Struct(int nAssentos){
 		int i;
-		assentos = new Assentos[nAssentos];
+		assentos = new int[nAssentos];
+		for(i = 0; i < nAssentos; i++){
+			assentos[i] = 0;
+		}
 		id  = 0;
 		task = 0;
-
 	}
+
+	public void setItens(int id, int task, int[] mapa){
+		this.id = id;
+		this.task = task;
+		this.assentos = mapa;
+	}
+
+	public int getId(){
+		return this.id;
+	}
+	public int getTask(){
+		return this.task;
+	}
+	public int[] getMapa(){
+		return this.assentos;
+	}
+
 }
