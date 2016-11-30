3ef265ac9013969485fd13c5d82835de874df866
wagnervieirafilho <waguinhosanjos@gmail.com>
2016-11-30 17:28:11 -0200

    Implementação dos método Insere e Remove

--- Trabalho2/Buffer.java
+++ Trabalho2/Buffer.java
@@ -12,6 +12,45 @@ class Buffer{
  		}
  	}
 
+ 	public synchronized void Insere (int id, int task, int[] mapa) {
+    		try {
+			while(count == N){
+			              System.out.println("Buffer cheio, bloqueando thread produtora ");
+			              this.wait();
+			              System.out.println("Buffer disponivel, liberando uma thread produtora..... ");
+			 }
+		               buffer[in].setItens(id, task, mapa);
+		               System.out.println("Item produzido == ");
+		               in = (in + 1) % N;
+		               count++;
+		               this.notify();
+		} 
+		catch (InterruptedException e) { }
+  	}
+
+  	public synchronized void Remove() {
+   		int id;
+   		int task;
+   		int[] map;
+
+   		try {
+			while(count == 0){
+			       System.out.println("Buffer vazio, bloquendo thread consumidora ");
+			       this.wait();
+			       System.out.println("Posição liberada para consumo, liberando thread consumidora..... ");
+			}
+			id = buffer[out].getId();
+			task = buffer[out].getTask();
+			map = buffer[out].getMapa();
+
+			System.out.println("Elemento consumido == ");
+			out = (out + 1) % N;
+			count--;
+			this.notify();
+		} 
+		catch (InterruptedException e) {}
+	}
+
 
 	
 }
