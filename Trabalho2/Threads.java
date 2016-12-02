fe04d7570acb4bc174faac519990707b7a400635
wagnervieirafilho <waguinhosanjos@gmail.com>
2016-12-02 00:33:58 -0200

    Threads produtoras quase prontas, falta preencher o buffer

--- Trabalho2/Threads.java
+++ Trabalho2/Threads.java
@@ -35,7 +35,8 @@ class Produtor1 extends Thread {
 
         // Método executado pelo thread
         public void run () {
-          System.out.println("Ta na produtora "+this.id);
+          this.a.visualizaAssentos();
+          v = this.a.alocaAssentoLivre(this.id);              //aloca assento aleatório e retorna vetor dizendo se foi possível alocar e qual assento foi alocado
           this.a.visualizaAssentos();
        }
 } 
@@ -44,6 +45,7 @@ class Produtor2 extends Thread {
         int id;
         Assentos a;
         Buffer buffer;
+        int t_assento;
 
         // Construtor
         Produtor2 (int id, Assentos a, Buffer b) {
@@ -54,7 +56,11 @@ class Produtor2 extends Thread {
 
         // Método executado pelo thread
         public void run () {
-              System.out.println("Ta na produtora "+this.id);
+              this.a.visualizaAssentos();
+              this.t_assento = 2;                                                 //assento que vai tentar alocar
+              this.a.alocaAssentoDado(t_assento, this.id);    // aloca o assento dado
+              this.a.visualizaAssentos();
+
         }
 } 
 
@@ -62,17 +68,28 @@ class Produtor3 extends Thread {
         int id;
         Assentos a;
         Buffer buffer;
+        int t_assento;
+        int[] v;
 
         // Construtor
         Produtor3 (int id, Assentos a, Buffer b) {
           this.id = id;
           this.a = a;
           this.buffer = b;
+          this.v = new int[2];
         }
 
         // Método executado pelo thread
         public void run () {
-              System.out.println("Ta na produtora "+this.id);
+            this.a.visualizaAssentos();
+            v = this.a.alocaAssentoLivre(this.id);    //aloca assento aleatório e retorna vetor que diz se foi ou não possivel alocar e qual assento foi alocado
+            if(v[0] != 0){                                              // se a alocação de fato ocorreu, t_assento recebe o assento que foi alocado
+                t_assento = v[1];
+                this.a.visualizaAssentos();
+                this.a.liberaAssento(t_assento, this.id); // libera assento que acabou de ser alocado
+            }
+            this.a.visualizaAssentos();
+
         }
 } 
 
@@ -90,6 +107,12 @@ class Produtor4 extends Thread {
 
         // Método executado pelo thread
         public void run () {
-            System.out.println("Ta na produtora "+this.id);
+             this.a.visualizaAssentos();
+              this.a.alocaAssentoDado(6,this.id);   //aloca assento 6 para cliente 4
+              this.a.alocaAssentoDado(7,this.id);   //aloca assento 7 para cliente 4
+              this.a.alocaAssentoDado(8,this.id);   //aloca assento 8 para cliente 4
+              this.a.liberaAssento(1, this.id);           // tenta liberar assento que não foi alocado pelo cliente 4, vai dar erro!
+              this.a.visualizaAssentos();
+            
         }
 } 
