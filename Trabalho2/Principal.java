d92d84ba959339c8f11c1d25efc098ef555d2403
wagnervieirafilho <waguinhosanjos@gmail.com>
2016-12-07 18:01:01 -0200

    versão final

--- Trabalho2/Principal.java
+++ Trabalho2/Principal.java
@@ -11,23 +11,56 @@ import java.io.IOException;
 class Principal{
 	static int nASSENTOS;
 	static String caminhoArqSaida;
+	static int P1= 10;
+	static int P2= 10;
+	static int P3= 10;
+	static int P4= 10;
 
 	public static void main(String[] args){
-
+		int i;
+		int id;
 		nASSENTOS = Integer.parseInt(args[1]);		//pega o numero de assentos da linha de comando
 		caminhoArqSaida = args[0];				//pega o caminho para o arquivo de log
 
 		Buffer buffer = new Buffer(nASSENTOS);		// Monitor
 		Assentos assentos = new Assentos(nASSENTOS, buffer);	// assentos
 
-		Consumidor cons;					// Consumidora
-		Produtor1 prod1;					// Produtores
-		Produtor2 prod2;
-		Produtor3 prod3;
-		Produtor4 prod4;
+		Consumidor cons;
+    		Produtor1[] prod1 = new Produtor1[P1];
+    		Produtor2[] prod2 = new Produtor2[P2];
+    		Produtor3[] prod3= new Produtor3[P3];
+    		Produtor4[] prod4 = new Produtor4[P4];
 
+		
 
-		prod1 = new Produtor1(1, assentos);		//inicia a thread que simula um cliente fazendo uma certa série de operações
+    		for (i=1; i<=P1; i++) {
+       			prod1[i-1] = new Produtor1(i,assentos);
+       			prod1[i-1].start();
+    		}
+		
+    		id = P1+1;
+		for (i=0; i<P2; i++) {
+       			prod2[i] = new Produtor2(id,assentos);
+       			id++;
+       			prod2[i].start();
+    		}
+
+    		for (i=0; i<P3; i++) {
+       			prod3[i] = new Produtor3(id,assentos);
+       			id++;
+       			prod3[i].start();
+    		}
+
+    		for (i=0; i<P4; i++) {
+       			prod4[i] = new Produtor4(id,assentos);
+       			id++;
+       			prod4[i].start();
+    		}
+
+    		cons = new Consumidor(0, assentos, caminhoArqSaida, buffer);	//inicia a thread consumidora
+		cons.start();
+
+		/*prod1 = new Produtor1(1, assentos);		//inicia a thread que simula um cliente fazendo uma certa série de operações
 		prod1.start();
 
 		prod2 = new Produtor2(2, assentos);		//inicia a thread que simula um cliente fazendo outra certa série de operações
@@ -40,12 +73,28 @@ class Principal{
 		prod4.start();
 
 		cons = new Consumidor(0, assentos, caminhoArqSaida, buffer);	//inicia a thread consumidora
-		cons.start();
+		cons.start();*/
 
-		try { prod1.join(); } catch (InterruptedException e) { return; }	// espera a thread terminar
+		/*try { prod1.join(); } catch (InterruptedException e) { return; }	// espera a thread terminar
 		try { prod2.join(); } catch (InterruptedException e) { return; }	// espera a thread terminar
 		try { prod3.join(); } catch (InterruptedException e) { return; }	// espera a thread terminar
-		try { prod4.join(); } catch (InterruptedException e) { return; }	//espera a thread terminar
+		try { prod4.join(); } catch (InterruptedException e) { return; }	//espera a thread terminar*/
+
+		for (i=0; i<P1; i++) {
+			try{ prod1[i].join(); }catch(InterruptedException e){return;}
+    		}
+
+    		for (i=0; i<P2; i++) {
+			try{ prod2[i].join(); }catch(InterruptedException e){return;}
+    		}
+
+    		for (i=0; i<P3; i++) {
+			try{ prod3[i].join(); }catch(InterruptedException e){return;}
+    		}
+
+    		for (i=0; i<P4; i++) {
+			try{ prod4[i].join(); }catch(InterruptedException e){return;}
+    		}
 		
 		buffer.setEstado(true);
 
