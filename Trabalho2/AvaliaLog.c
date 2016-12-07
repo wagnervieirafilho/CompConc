ed68b9f1024a0c654bc705addc1ca3a6d10a501f
wagnervieirafilho <waguinhosanjos@gmail.com>
2016-12-07 19:16:39 -0200

    Começãndo

---
+++ Trabalho2/AvaliaLog.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char const *argv[])
+{
+	char caminho[300];
+	int* v;
+	int id;
+	int task;
+	int assento;
+	int nAssentos;
+
+	FILE *arq;
+
+	strcpy(caminho, argv[1]);
+
+	arq = fopen(caminho, "r");
+	if(arq == NULL)
+	    printf("Erro, nao foi possivel abrir o arquivo\n");
+	else{
+		fgets(, sizeof(info), arq)
+	}
+
+	fclose(arq);
+
+
+	return 0;
+}
