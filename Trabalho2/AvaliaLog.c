98117e96c480d319b6d2dc3354f7a2c72ccb1440
wagnervieirafilho <waguinhosanjos@gmail.com>
2016-12-08 00:17:40 -0200

    ainda não faz muita coisa

--- Trabalho2/AvaliaLog.c
+++ Trabalho2/AvaliaLog.c
@@ -2,15 +2,24 @@
 //Avaliador do log de saída
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main(int argc, char const *argv[])
 {
 	char caminho[300];
+	char linha[300];
+	char elemento[10] = "";
+
 	int* v;
+	int* v2;
+
 	int id;
 	int task;
 	int assento;
 	int nAssentos;
+
+	int i;
+	int cont;
 
 	FILE *arq;
 
@@ -20,7 +29,17 @@ int main(int argc, char const *argv[])
 	if(arq == NULL)
 	    printf("Erro, nao foi possivel abrir o arquivo\n");
 	else{
-		fgets(, sizeof(info), arq)
+		fgets(linha, sizeof(linha), arq);		// pega a quantidade de assentos
+		nAssentos = atoi(linha);			// transgorma string pra int
+		v = malloc(sizeof(int) * nAssentos);	// aloca o vetor de assentos
+		for(i = 0; i < nAssentos; i++){
+			v[i] = 0;		// inicializa o vetor com 0
+		}
+
+		while( (fgets(linha, sizeof(linha), arq))!=NULL ){
+			/* Aqui tem que separar os elementos e atribuir a id, task e assento*/
+		}	
+
 	}
 
 	fclose(arq);
