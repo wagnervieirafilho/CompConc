13ed3ec2e45dd22cfc1a10516a8130d5545ad3ff
wagnervieirafilho <waguinhosanjos@gmail.com>
2016-12-08 23:28:45 -0200

    Versão Final comentada

--- Trabalho2/AvaliaLog.c
+++ Trabalho2/AvaliaLog.c
@@ -45,8 +45,8 @@ int main(int argc, char const *argv[])
 	else{
 		fgets(linha, sizeof(linha), arq);		// pega a quantidade de assentos
 		nAssentos = atoi(linha);			// transgorma string pra int
-		v = malloc(sizeof(int) * nAssentos);	// aloca o vetor de assentos
-		v2 = malloc(sizeof(int) * nAssentos);	// aloca o vetor de assentos
+		v = malloc(sizeof(int) * nAssentos);	// aloca o vetor de assentos em que serão feitas as modificações para posterior comparação
+		v2 = malloc(sizeof(int) * nAssentos);	// aloca o vetor de assentos que será pego do arquivo e comparado com o vetor  V após as modificações serem feitas
 		for(i = 0; i < nAssentos; i++){
 			v[i] = 0;		// inicializa o vetor com 0
 		}
@@ -107,30 +107,30 @@ int main(int argc, char const *argv[])
 				aux = i;
 			}
 /*--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
-			if(task == 1){
+			if(task == 1){		// visualiza o mapa de assentos e compara
 				visualizaAssentos();
 				if(comparaVetores() == FALSE)
 					printf("Falha! linha %d\n", linhaDoArquivo);
 				else
 					printf("Sucesso!\n");
 			}
-			if(task == 2){
+			if(task == 2){		//  simula uma alocação aleatória (que aqui já não é mais aleatória por que no arquivo ja está salvo qual assento aleatório foi escolhido, ele é apenas usado aqui)
 				alocaAssentoLivre(id, assento);
-				if(comparaVetores() == FALSE)
+				if(comparaVetores() == FALSE)	// compara os vetores
 					printf("Falha! linha %d\n", linhaDoArquivo);
 				else
 					printf("Sucesso!\n");
 			}
 			if(task == 3){
-				alocaAssentoDado(id, assento);
-				if(comparaVetores() == FALSE)
+				alocaAssentoDado(id, assento);	// simula uma alocação de um assento específico
+				if(comparaVetores() == FALSE)	// compara os vetores
 					printf("Falha! linha %d\n", linhaDoArquivo);
 				else
 					printf("Sucesso!\n");
 			}
 			if(task == 4){
-				liberaAssento(id, assento);
-				if(comparaVetores() == FALSE)
+				liberaAssento(id, assento);		// simula a liberação de um assento dado
+				if(comparaVetores() == FALSE)	// compara os vetores
 					printf("Falha! linha %d\n", linhaDoArquivo);
 				else
 					printf("Sucesso!\n");
@@ -147,56 +147,47 @@ int main(int argc, char const *argv[])
 void visualizaAssentos(){
 	// não altera o vetor
 }
-void alocaAssentoLivre(int id, int assento){
+void alocaAssentoLivre(int id, int assento){	// tenta fazer a alocação de um assento "Aleatório" que na verdade não é aleatório porque já foi pego do arquivo
 	int posicao = assento-1;
 	int i;
 	int assentosEsgotados = 1;
 
 	for (i = 0; i < nAssentos; i++){
 		if(v[i] == 0)
-			assentosEsgotados = 0;
+			assentosEsgotados = 0;	// verifica se os assentos estão  esgotados --> 0: não está esgotado; 1: esgotados
 	}
 
-	if(assento <= nAssentos){
-		if(assentosEsgotados == 0){
-			if(v[posicao] == 0)
-				v[posicao] = id;
+	if(assento <= nAssentos){			// se o assento escolhido estiver dentro do número de assentos
+		if(assentosEsgotados == 0){	 	// se houver  assentos disponíveis
+			if(v[posicao] == 0)		// se o assento em questão estiver disponível
+				v[posicao] = id;	// aloca
 		}
 	}
 
 }
-void alocaAssentoDado(int id, int assento){
+void alocaAssentoDado(int id, int assento){	// tenta fazer a alocação de um assento específico
 	int posicao = assento-1;
-	int i;
-	int assentosEsgotados = 1;
 
-	for (i = 0; i < nAssentos; i++){
-		if(v[i] == 0)
-			assentosEsgotados = 0;
-	}
-
-	if(assento <= nAssentos){
-		if(assentosEsgotados == 0){
-			if(v[posicao] == 0)
-				v[posicao] = id;
-		}
+	if(assento <= nAssentos){		// se o assento escolhido estiver dentro do número de assentos
+		if(v[posicao] == 0)		// se o assento em questão estiver disponível
+			v[posicao] = id;	// aloca
 	}
 }
 void liberaAssento(int id, int assento){
 	int posicao = assento-1;
-	if(v[posicao] != 0){
-		if(v[posicao] == id){
-			v[posicao] = 0;
+	if(v[posicao] != 0){			// se o assento esiver ocupado
+		if(v[posicao] == id){		// se o usuário que está tentando desalocar for  o mesmo que alocou
+			v[posicao] = 0;		// libera
 		}
 	}
 
 }
 int comparaVetores(){
-	int avalia = TRUE;
+	int avalia = TRUE;		// TRUE significa que tudo está dentro do normal
 	int i;
-	for (i = 0; i < nAssentos; i++){
-		if(v[i] != v2[i])
-			avalia = FALSE;
+	for (i = 0; i < nAssentos; i++){	// para cada posição dos dois vetores
+		if(v[i] != v2[i])		// teste se eles são iguais, se pelo menos uma posição de um vetor for diferente da mesma posição do outro vetor
+			avalia = FALSE;// avalia recebe FALSE, significando que algo deu errado
 	}
-	return avalia;
+	return avalia;			// retorna a avaliação
 }
